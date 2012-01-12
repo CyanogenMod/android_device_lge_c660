ca079645a8c879134756d3f141adfbe9eb4ded5f
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-01-12 22:22:49 +0000

    c660: Change fbcon-related workaround
    Removing the module in runtime was causing some instability in the
    kernel video layer, so leave it in, and avoid detection by sf
    
    Change-Id: Ia52445a3ffdf3fd9c3f25cd5b5d33b1291957454

--- c660.mk
+++ c660.mk
@@ -43,8 +43,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/kbd_pp2106.kcm.bin:system/usr/keychars/kbd_pp2106.kcm.bin
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/02removefbcon:system/etc/init.d/02removefbcon \
-    $(LOCAL_PATH)/prebuilt/fbconsole.ko:system/lib/modules/fbconsole.ko \
     $(LOCAL_PATH)/chargemode/chargerlogo:root/sbin/chargerlogo \
     $(LOCAL_PATH)/chargemode/images/battery_ani_01.rle:root/chargerimages/battery_ani_01.rle \
     $(LOCAL_PATH)/chargemode/images/battery_ani_02.rle:root/chargerimages/battery_ani_02.rle \
